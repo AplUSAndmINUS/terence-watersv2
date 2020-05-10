f3666b580465765133d7dacf673712e19dd38927
Terence Waters <terence.waters@imail.org>
2020-05-09 20:24:20 -0600

    feat(TC-13): adding the colors and variables for standard spacing

---
+++ src/modules/colors.ts
@@ -0,0 +1,23 @@
+// light mode colors
+export const ColorsLt = {
+  black: '#383838',
+  white: '#FFF',
+  blueHeading: '#5B799B',
+  blueSubheading: '#748BA5',
+  blueInputField: '#748BA5',
+  card: '#FFF',
+  cardCal: '#5B799B',
+  cardCalInactive: '#E3E3E3'
+};
+
+// dark mode colors
+export const ColorsDk = {
+  white: '#FFF',
+  whiteSubheading: 'rgba(255,255,255,0.7)',
+  blueHeading: '#5B799B',
+  blueSubheading: '#748BA5',
+  blueInputField: '#748BA5',
+  card: '#5C5C5C',
+  cardCal: '#748BA5',
+  cardCalInactive: '#434242'
+};
---
+++ src/modules/variables.ts
@@ -0,0 +1,84 @@
+export const BorderRadius = {
+  none: 0,
+  small: 3,
+  base: 5,
+  large: 10,
+  xLarge: 24,
+  full: 500
+};
+
+export const BoxShadow = {
+  boxShadow: 
+};
+
+export const FontFamily = {
+  primary: 'Helvetica Neue',
+  secondary: 'Roboto',
+  tertiary: 'Proxima Nova Extrabold'
+}
+
+export const FontSizeDesktop = {
+  xsmall: 10,
+  small: 12,
+  base: 14,
+  medium: 15,
+  large: 16,
+  smallButton: 18,
+  smallHeading: 20,
+  mediumHeading: 24,
+  buttonLarge: 27,
+  largeHeading: 28,
+  skills: 30,
+  subheading: 48,
+  heading: 60,
+  title: 96
+};
+
+export const FontSizeMobile = {
+  xsmall: 10,
+  small: 12,
+  base: 14,
+  medium: 15,
+  large: 16,
+  smallButton: 18,
+  smallHeading: 20,
+  mediumHeading: 24,
+  buttonLarge: 27,
+  largeHeading: 28,
+  skills: 30,
+  subheading: 48,
+  heading: 60,
+  title: 96
+};
+
+export const FontWeight = {
+  light: '300',
+  normal: '400',
+  semibold: '600',
+  bold: '700'
+};
+
+export const IconSize = {
+  xsmall: 8,
+  small: 16,
+  base: 24,
+  large: 32,
+  xLarge: 48
+};
+
+export const Kerning = {
+  none: 0,
+  subheading: -15,
+  heading: -25,
+  title: 50
+};
+
+export const Spacing = {
+  none: 0,
+  small: 4,
+  medium: 8,
+  large: 16,
+  xLarge: 32,
+  xxLarge: 100,
+  xxxLarge: 200
+};
