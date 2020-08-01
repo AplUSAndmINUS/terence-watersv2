2ab3fd1366facf33161708caaa3929849587e063
12157591+AplUSAndmINUS@users.noreply.github.com <terence.waters@imail.org>
2020-08-01 16:46:23 -0600

    added button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { MuiButton } from '../../theme/material-ui';
+
+const Button = ({ children }) => {
+  return <MuiButton>{children}</MuiButton>;
+};
+
+export default Button;
