dd9a3f0de3fea4688a2fb2ded00cb3f5c5d51856
Terence Waters <terence.waters@imail.org>
2020-08-08 18:23:24 -0600

    updated menu button initial component

---
+++ src/components/menu/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Menu = () => {
+  return <h1>My menu button</h1>;
+}
+
+export default Menu;
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Menu from '../../components/menu';
+
 const Home = () => {
   return (
     <div className="home">
+      <Menu />
       <h1>This is the main Home page!</h1>
-    </div>)
-  ;
-}
+    </div>
+  );
+};
 
 export default Home;
